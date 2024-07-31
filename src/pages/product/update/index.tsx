0f6b7a1df893638dfaaa2a6efbe78154b0aa6b86
Shalahuddin Ahmad Aziz <shalahuddin.ahmad.aziz@mandalafinance.com>
2024-07-31 17:31:17 +0700

    update integration

--- src/pages/product/update/index.tsx
+++ src/pages/product/update/index.tsx
@@ -1,11 +1,85 @@
-import { UpdateProductPageProps } from "@/types";
 import React from "react";
+import { connect } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import Select from "react-select";
+
+import { UpdateProductPageProps, UpdateProductRequest } from "@/types";
+import { MainButton, Navbar, TextInput } from "@/components";
+import { updateProduct, updateProductReset } from "@/redux/product/actions";
+import { getAllCategories } from "@/redux/category/actions";
 import { useUpdate } from "./useUpdate";
 
 const UpdateProductPage: React.FC<UpdateProductPageProps> = (props) => {
   const update = useUpdate(props);
 
-  return <div>UpdateProductPage</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <span className="mb-5 text-black font-bold">Update Product</span>
+
+        <form className="space-y-6" action="#" method="POST">
+          <TextInput
+            title="Name"
+            placeholder="Input your name..."
+            onChange={(e) => update.onChange(e, "name")}
+            value={update.form.name}
+          />
+          <TextInput
+            title="Description"
+            placeholder="Input your description..."
+            onChange={(e) => update.onChange(e, "description")}
+            value={update.form.description}
+          />
+          <TextInput
+            title="Image Url"
+            placeholder="Input your image Url..."
+            onChange={(e) => update.onChange(e, "imageUrl")}
+            value={update.form.imageUrl}
+          />
+          <Select
+            options={update.options}
+            onChange={(e) => update.onSelect(e)}
+            value={update.options.find(
+              (opt) => opt.value === update.form.categoryId
+            )}
+          />
+          <TextInput
+            type="number"
+            title="Stock"
+            placeholder="Input your stock..."
+            onChange={(e) => update.onChange(e, "stock")}
+            value={update.form.stock}
+          />
+          <MainButton
+            loading={update.loading}
+            disabled={update.loading || update.disabled}
+            title={"Update"}
+            onClick={update.onUpdate}
+          />
+        </form>
+      </div>
+    </>
+  );
 };
 
-export default UpdateProductPage;
+const mapStateToProps = (state: RootState) => ({
+  updateProductLoading: state.product.updateProductLoading,
+  updateProductResponse: state.product.updateProductResponse,
+  updateProductError: state.product.updateProductError,
+
+  getAllCategoriesLoading: state.category.getAllCategoriesLoading,
+  getAllCategoriesResponse: state.category.getAllCategoriesResponse,
+  getAllCategoriesError: state.category.getAllCategoriesError,
+
+  session: state.auth.session,
+});
+
+const mapDispatchToProps = (dispatch: AppDispatch) => ({
+  updateProduct: (payload: UpdateProductRequest) =>
+    dispatch(updateProduct(payload)),
+  updateProductReset: () => dispatch(updateProductReset()),
+  getAllCategories: () => dispatch(getAllCategories()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UpdateProductPage);
--- src/pages/product/update/useUpdate.ts
+++ src/pages/product/update/useUpdate.ts
@@ -1,4 +1,7 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useRouter } from "next/router";
 import { UpdateProductPageProps } from "@/types";
+import { RESPONSE_STATUS } from "@/redux/constants";
 
 export const useUpdate = (props: UpdateProductPageProps) => {
   const {
@@ -8,8 +11,108 @@ export const useUpdate = (props: UpdateProductPageProps) => {
     updateProductResponse,
     updateProductReset,
 
+    getAllCategories,
+    getAllCategoriesLoading,
+    getAllCategoriesError,
+    getAllCategoriesResponse,
+
     session,
   } = props;
+  const router = useRouter();
+  const { query } = router;
 
-  return {};
+  const [form, setForm] = useState({
+    id: 0,
+    name: "",
+    description: "",
+    imageUrl: "",
+    categoryId: 0,
+    stock: 0,
+  });
+
+  const isInvalid = useMemo(() => {
+    return (
+      !form.name ||
+      !form.description ||
+      !form.imageUrl ||
+      !form.categoryId ||
+      !form.stock
+    );
+  }, [form]);
+
+  const options = useMemo(() => {
+    if (getAllCategoriesResponse?.data) {
+      return getAllCategoriesResponse?.data?.map((cat) => {
+        return {
+          label: cat.name,
+          value: cat.id,
+        };
+      });
+    }
+    return [];
+  }, [getAllCategoriesResponse]);
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>, props: string) => {
+      const value = e.target.value;
+      setForm({
+        ...form,
+        [props]: value,
+      });
+    },
+    [form]
+  );
+
+  const onSelect = useCallback(
+    (e: any) => {
+      setForm({
+        ...form,
+        categoryId: e.value,
+      });
+    },
+    [form]
+  );
+
+  const onUpdate = useCallback(() => {
+    if (isInvalid) {
+      return alert("Fill all of the form");
+    }
+    updateProduct(form);
+  }, [form, isInvalid]);
+
+  useEffect(() => {
+    if (!session?.token) {
+      window.location.href = "/login";
+    }
+    getAllCategories();
+  }, [session]);
+
+  useEffect(() => {
+    if (query.data) {
+      const data = JSON.parse(query.data as any);
+      setForm(data.product);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    if (updateProductResponse.status === RESPONSE_STATUS.SUCCESS) {
+      updateProductReset();
+      alert("Update Product Data Success");
+      router.push("/product");
+    }
+    if (updateProductError) {
+      updateProductReset();
+      alert("Update Product Data Failed");
+    }
+  }, [updateProductResponse]);
+
+  return {
+    loading: updateProductLoading || getAllCategoriesLoading,
+    disabled: isInvalid,
+    form,
+    options,
+    onChange,
+    onSelect,
+    onUpdate,
+  };
 };
